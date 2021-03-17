b8eea4b06d7d3bae363ec241d8da80fa8b472031
MarcoAR1 <marcorivero.mr26@gmail.com>
2021-03-17 16:06:35 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(require("./routes/index"));
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.listen(port, () => {
+  console.log("Server on port 3000");
+});
